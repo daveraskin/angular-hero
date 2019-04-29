61d6a4f7ce230fc365a19c3726f05d8932ebc41d
David Raskin <monitorbandtestsite@gmail.com>
2019-04-29 11:22:46 -0700

    changing from starter app to Tour of Heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'My First Angular App in a While';
+  title = 'Tour of Heroes';
 }
